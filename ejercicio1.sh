aa07c3e01d409f5c43dc65f97355f90fa5336687
Julio <JulioToboso@gmail.com>
2019-10-02 16:55:08 +0200

    Update ejercicio1.sh

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -3,4 +3,13 @@
 #  Create directory /tmp/d
 mkdir /tmp/d
 
-#  Move file to 
+#  Move file to tmp and rename
+cd /tmp/d
+touch f
+mv f /tmp
+cd /tmp
+mv f my_file
+
+#  Delete directory /tmp/d with all content
+rmdir -r /tmp/d
+
