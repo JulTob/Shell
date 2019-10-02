f4ebe90d6e1c448758346459ef50c8698c62bad4
Julio <JulioToboso@gmail.com>
2019-10-02 17:10:57 +0200

    Update ejercicio2.sh

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -1,1 +1,15 @@
+#!/bin/sh
 
+
+mkdir /tmp/folder1
+
+for index in 1 2 3; 
+do 
+  file=/tmp/file$index
+  touch $file
+  echo "Hello World" > $file
+  
+done
+
+
+echo " and goodbye" >> /tmp/folder1/file2
