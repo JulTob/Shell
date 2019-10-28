0d7939df9c5e7a96548369aafa1727ddea361cc0
Julio <JulioToboso@gmail.com>
2019-10-28 13:07:26 +0100

    Create ejercicio5_info.sh

---
+++ ejercicio5_info.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Information
+echo "Nombre:"
+whoami
+echo "Directorio de mi cuenta: "
+home
+echo "Directorio actual:"
+pwd
+echo "Fecha: "
+date
+echo "*****************************"
+echo "Variables de entorno"
+echo '$HOME:'"$HOME"
+echo '$PATH:'"$PATH"
+echo '$SHELL:'"$SHELL"
+echo '$USERNAME:'"$USERNAME"
+echo "****************************"
