a1e8be6675cd7e4619e719dce7ac15165c0683dc
Julio <JulioToboso@gmail.com>
2019-12-07 15:20:19 +0100

    Create MakeProgramScript.sh

---
+++ MakeProgramScript.sh
@@ -0,0 +1,14 @@
+# navigate to home dir
+$ cd ~
+
+# create main.c file
+$ touch main.c
+
+# edit main.c (write program)
+$ nano main.c
+
+# compile (and link) program
+$ gcc main.c -o helloWorld
+
+# run program
+$ ./helloWorld
